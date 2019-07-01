fee46b1df99a7d4c5805c861116eaedf5517d55a
Alessandra Vertrees <avertrees447@gmail.com>
2019-07-01 15:16:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(string)
   new = []
   i = 0
   while i < source_array.length do
-    new.push( source_array[i] * source_array[i] ) # <==
+    new.push( source_array[i].each ) # <==
     i += 1
   end
   return new
