1c4678ea7f455b072ea29a3ab72c0052950c3cc5
Alessandra Vertrees <avertrees447@gmail.com>
2019-07-01 15:15:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,10 @@
 def reverse_each_word(string)
-  arr = string.split(" ")
-  while 
+  source_array = string.split(" ")
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] * source_array[i] ) # <==
+    i += 1
+  end
+  return new
 end
