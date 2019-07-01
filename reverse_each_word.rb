ceb5e4e8db9f6e0cbf3aaa870bb28dfc7424d616
Alessandra Vertrees <avertrees447@gmail.com>
2019-07-01 15:26:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
 def reverse_each_word(string)
   source_array = string.split(" ")
 
-  new = source_array.each{ |w| w.reverse  }
+  new = source_array.each{ 
+    |w| w.reverse  
+  }
 
   return new.join(" ")
 end
